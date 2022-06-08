087a0ba9f4de02cd7d13951c79264411e52ec55e
BassIsKey <106938686+BassIsKey@users.noreply.github.com>
2022-06-08 18:56:35 -0400

    Update classPractice.py

--- classPractice.py
+++ classPractice.py
@@ -1,11 +1,11 @@
 class Robot:
-  def __init__(self, name, color, weight):
-    self.name = name
-    self.color = color
-    self.weight = weight
+    def __init__(self, name, color, weight):
+        self.name = name
+        self.color = color
+        self.weight = weight
     
     def introduce_self(self):
-      print(f"My name is {self.name}")
+        print(f"My name is {self.name}")
 
 r1 = Robot("Tom", "red", 30)
 r2 = Robot("Jerry", "blue", 40)
